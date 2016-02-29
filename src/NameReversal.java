252d0b0e3cb54d9845c992f2c4a07e4abda4e353
unknown <674843024@ntdm-134-12.hcdsb.edu.on.ca>
2016-02-29 13:56:50 -0500

    lol ezest assignment of lif

--- src/NameReversal.java
+++ src/NameReversal.java
@@ -13,15 +13,18 @@ public class NameReversal {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String name = "";
+		System.out.println("What is your name?\n");
+		name = in.nextLine(); //I am an idiot lol why did I initially put this after the assignment of revName
 		String revName = getStringOfLength(name.length());
-		name = in.next();
 		if (revName.length() == 0) {
 			System.out.println("An error has occured! Setting name to John Smith!");
 			name = "John Smith";
-			revName = "          ";
 		}
+		System.out.println(name.length());
+		//revName = name.substring(name.length()-1, 0);
 		for (int i = 0; i<name.length(); i++) {
-			//todo
+			revName += name.charAt((name.length()-1)-i);	
 		}
+		System.out.println("Reversed, your name is " + revName);
 	}
 }
